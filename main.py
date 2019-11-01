210fb1018a17955cd4aab8342609859ec6c9507a
mr bones <bones@localhost.localdomain>
2019-11-02 00:26:04 +0300

    Initial commit

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
+import statistics
+
 print("Hello World")
+
+#name
+print("My name is Stan.")
+
+#a string with smilling emoji
+print("I am 20+ years of age. Young, right?","\U0001F600")
+
+#maximum number in the list
+b=[12,4,56,17,8,99]
+print("the maximum number: ",max(b))
+
+#the mean
+
+a=[12,4,56,17,8,99]
+x=statistics.mean(a)
+print("the mean is:",x)
+
+#Alphabets
+x="\n" .join(['A for apple','B for Boy','C for Cow','.....' ,'Z for Zebra'])
+print(x)
+
+
+
